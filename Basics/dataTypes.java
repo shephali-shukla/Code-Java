e9a304ff410a76b4c93ececab1d012e57ced8070
Shephali <shephalishukla1@gmail.com>
2023-08-13 01:05:13 +0530

    Data types in java

---
+++ Basics/dataTypes.java
@@ -0,0 +1,18 @@
+public class dataTypes {
+    public static void main(String[] args) {
+     
+        //Fundamental | Primitive
+
+        byte type1 = 0;
+        short type2 = 11;
+        int type3 = 123;
+        long type4 = 1234;
+
+        float type5 = 0.1f;
+        double type6 = 12.8;
+
+        /*Non-Primitive
+        Array, String*/
+
+    }
+}
