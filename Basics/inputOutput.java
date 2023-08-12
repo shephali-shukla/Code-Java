53e1c4c60a1b0a9859897239016e4dedf3114950
Shephali <shephalishukla1@gmail.com>
2023-08-12 23:29:03 +0530

    Initial commit

---
+++ Basics/inputOutput.java
@@ -0,0 +1,12 @@
+class inputOutput{
+    public static void main(String[] args) {
+        
+        int i; //Declaration
+        i = 7; //Initialization
+        //Both can be done in single line: int i = 7.
+
+        System.out.println("The value of i = " + i);
+        
+    }
+}
+
