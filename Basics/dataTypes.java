48dca667d117f8bdc95b2af0b59fc9f965c449b6
Shephali <shephalishukla1@gmail.com>
2023-08-13 01:05:57 +0530

    Added print statements

--- Basics/dataTypes.java
+++ Basics/dataTypes.java
@@ -11,6 +11,13 @@ public class dataTypes {
         float type5 = 0.1f;
         double type6 = 12.8;
 
+        System.out.println(type1);
+        System.out.println(type2);
+        System.out.println(type3);
+        System.out.println(type4);
+        System.out.println(type5);
+        System.out.println(type6);
+
         /*Non-Primitive
         Array, String*/
 
