d6479a312bb478064d62e6801fafa3cdd6de046a
Shephali <shephalishukla1@gmail.com>
2023-08-17 00:47:48 +0530

    First pattern code

---
+++ Pattern/POne.java
@@ -0,0 +1,12 @@
+class POne{
+    public static void main(String[] args) {
+        
+        int i,j;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
