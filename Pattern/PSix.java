8f64d4bef73e7e47a6c2ac865a4ad02f0cef6356
Shephali Shukla <109907467+shephali-shukla@users.noreply.github.com>
2023-08-22 23:32:18 +0530

    Pattern Six

---
+++ Pattern/PSix.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+class PThree {
+    public static void main(String[] args) {
+        
+        for(char a='A';a<='E';a++){
+		      for(char b='A';b<=a;b++){
+		        System.out.print(b);
+		      }
+		      System.out.println();
+		   }
+    }    
+}
