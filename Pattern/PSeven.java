09caeb15b906a47cb808809686c3733e0e637eca
Shephali Shukla <109907467+shephali-shukla@users.noreply.github.com>
2023-08-25 00:00:40 +0530

    Pattern Seven

---
+++ Pattern/PSeven.java
@@ -0,0 +1,14 @@
+package Pattern;
+
+class PThree {
+    public static void main(String[] args) {
+        
+        char i,j;
+        for(i='E;i>='A;i--){
+            for(j='A;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }    
+}
