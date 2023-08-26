7f9ea6154d8560d4b99f5f01d212eed57d72e206
Shephali Shukla <shephalishukla1@gmail.com>
2023-08-27 00:58:02 +0530

    Added single inverted commmas

--- Pattern/PSeven.java
+++ Pattern/PSeven.java
@@ -4,8 +4,8 @@ class PThree {
     public static void main(String[] args) {
         
         char i,j;
-        for(i='E;i>='A;i--){
-            for(j='A;j<=i;j++){
+        for(i='E';i>='A';i--){
+            for(j='A';j<=i;j++){
                 System.out.print(j);
             }
             System.out.println();
