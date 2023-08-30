e11b0d7bcfbee408c784fa3b929fff3c5114fa15
Shephali Shukla <shephalishukla1@gmail.com>
2023-08-30 20:55:47 +0530

    Pattern Twelve

---
+++ Pattern/PTwelve.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class PTwelve {
+    public static void main(String[] args) {
+        
+       for(char i='E';i>='A';i--){
+            for(char j=i;j<='E';j++){
+                System.out.print(j+" ");  
+            }
+            System.out.println();
+        } 
+    }
+}
