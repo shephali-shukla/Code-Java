7f52c28f1fbbfdd71bfecdbd2161de4cc67c6bc6
Shephali Shukla <shephalishukla1@gmail.com>
2023-08-30 20:56:01 +0530

    Pattern thirteen

---
+++ Pattern/PThirteen.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class PThirteen {
+    public static void main(String[] args) {
+        
+        for(char i='A';i<='E';i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
