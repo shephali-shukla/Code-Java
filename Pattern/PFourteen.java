4965727cdf19e3d7b6090a6c0e0e0b2ae9368c90
Shephali Shukla <shephalishukla1@gmail.com>
2023-08-31 17:31:30 +0530

    Pattern Fourteen

---
+++ Pattern/PFourteen.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+class PFour {
+    public static void main(String[] args) {
+        
+        for(int i=5;i>=1;i--){
+            for(int j=i;j<5;j++){
+                System.out.print(" S");
+            }
+
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }    
+}
