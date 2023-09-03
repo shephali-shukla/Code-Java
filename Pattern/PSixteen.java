9bff8f12165db69b22a108fd6d74ad187141370c
Shephali Shukla <shephalishukla1@gmail.com>
2023-09-03 23:55:55 +0530

    Pattern sixteen

---
+++ Pattern/PSixteen.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+public class PSixteen {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+
+            if(i<=3){
+                for(int j=3;j>=0;j--){
+                    
+                }
+            }
+            System.out.println();
+        }
+    }
+}
