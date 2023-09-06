c29fa603dbe9aa140119c8397949860e33fdd1da
Shephali Shukla <shephalishukla1@gmail.com>
2023-09-06 23:41:43 +0530

    Pattern Eighteen

---
+++ Pattern/PEighteen.java
@@ -0,0 +1,42 @@
+package Pattern;
+
+public class PEighteen {
+    public static void main(String[] args) {
+        
+        //Upper-triangle
+        for(int i=1;i<=5;i++){
+            
+            for(int j=5;j>i;j--){
+                System.out.print("_");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+
+            for(int j=i-1;j>=1;j--){
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        //Lower-triangle
+        for(int i=5;i>=1;i--){
+            
+            for(int j=i;j<5;j++){
+                System.out.print("_");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+
+            for(int j=i-1;j>=1;j--){
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
