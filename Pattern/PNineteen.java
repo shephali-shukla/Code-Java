feed0eac9c0eef64c8a50c0e4f6c391643da83ac
Shephali Shukla <shephalishukla1@gmail.com>
2023-09-09 01:16:41 +0530

    Added code for lower half

--- Pattern/PNineteen.java
+++ Pattern/PNineteen.java
@@ -3,6 +3,7 @@ package Pattern;
 public class PNineteen {
     public static void main(String[] args) {
         
+        //Upper-half
         for(int i=5;i>=1;i--){
 
             for(int j=1;j<=i;j++){
@@ -10,10 +11,34 @@ public class PNineteen {
             }
 
             for(int j=i;j<5;j++){
-                System.out.print("_");
+                System.out.print(" ");
             }
 
-            //for(int j=)
+            for(int j=5;j>i;j--){
+                System.out.print(" ");
+            }
+
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        //Lower-half
+        for(int i=1;i<=5;i++){
+            
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+
+            for(int j=i;j<5;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=i;j<5;j++){
+                System.out.print(" ");
+            }
 
             for(int j=i;j>=1;j--){
                 System.out.print(j);
