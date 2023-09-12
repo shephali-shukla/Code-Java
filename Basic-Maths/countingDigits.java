a6b31aa3012da7d79fdfdbf54198cae8f6a0ff22
Shephali Shukla <shephalishukla1@gmail.com>
2023-09-12 23:33:40 +0530

    Included other solutions

--- Basic-Maths/countingDigits.java
+++ Basic-Maths/countingDigits.java
@@ -1,6 +1,7 @@
 public class countingDigits {
     public static void main(String[] args) {
-        
+
+        //Solution 1: while loop
         int n=76546;
         int count = 0;
 
@@ -8,7 +9,16 @@ public class countingDigits {
             n /= 10;
             count++;
         }
-
         System.out.println("Digits present = " + count);
+
+
+        //solution 2: convert int to String & use function
+        int n1=76546;
+        String val = String.valueOf(n1); // Integer.toString(n1);
+
+        System.out.println("Sol 2 = " + val.length());
+
+        //Solution 3:Using log10()+1
+
     }
 }
